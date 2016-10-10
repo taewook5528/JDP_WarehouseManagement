7bf39df2577c69fcf249338e97e282f419e86224
윤종연 <0417yjy@naver.com>
2016-10-10 13:49:09 +0900

    헤드 클래스 초고 작성 (내부에 Request클래스 존재)

---
+++ src/Head.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Head {
+	//내부 주문 접수 클래스
+	private class Request {
+		private String storeName;
+		private String stockName;
+		private int amount;
+		private boolean confirmed; //주문 접수 여부
+		
+		public Request(String storeName, String stockName, int amount) {
+			this.storeName = storeName;
+			this.stockName = stockName;
+			this.amount = amount;
+			this.confirmed = false;
+		}
+	}
+	
+	String id, password;
+	private ArrayList<Warehouse> warehouses = new ArrayList<Warehouse>(); //창고 어레이리스트
+	private ArrayList<Store> stores = new ArrayList<Store>(); //가게 어레이리스트
+	
+	public void showDetail(Warehouse obj) {
+		
+	}
+	
+	public void showDetail(Store obj) {
+		
+	}
+	
+	public void calculate(Request r) {
+		
+		/* calculating ... */
+		
+		r.confirmed = true;
+	}
+}
